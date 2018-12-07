ac297603d30faacb6187cf6008db18f843495449
Felix Kihara <35260781+fkinaro@users.noreply.github.com>
2018-12-07 20:59:22 +0300

    Send weather forecast to email automatically

---
+++ mail.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# Script to send the weather forecasts to email
+import smtplib
+import json
+import sys
+
+
+# read email from file
+def email_reader(email_file):
+    '''Function to read te email data from txt file'''
+    with open('email.txt', 'r') as mail:
+        pass
+    pass
+
+
+# read password from file
+def password_reader(key_file):
+    '''Function to read the password for an email account'''
+    with open('password.txt', 'r') as pwd:
+        pass
+    pass
+
+
+
