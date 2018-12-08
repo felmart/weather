a5324894e8733a3c82400fc6ddfe7047cbd69f48
Felix Kihara <35260781+fkinaro@users.noreply.github.com>
2018-12-08 05:59:49 +0300

    Gets weather data. TODO: Correctly parse the response

--- weather.py
+++ weather.py
@@ -13,39 +13,28 @@ if len(sys.argv) < 2:
 
 location = ' '.join(sys.argv[1:])
 
-
-# function to read key data from file
-def key_reader(keyfile):
-    '''
-    Read the key data from a text file rather than rely on user input
-    Store the key as a variable
-    '''
-    with open('key.txt', 'r') as key:
-        api_key = str(key.read())
-    return api_key
-
+# read api key from file
+api_key = ''
+with open('key.txt', 'r') as key:
+    api_key = key.read()
 
 # download the json data from openweather
-# replace 'yourapikeyshouldbeinsertedhere' with your OWM API key
-def url_builder(location, api):
+def url_builder(api):
     '''Build the full url'''
-    url = 'http://api.openweathermap.org/data/2.5/forecast?q=%s&APPID=yourapikeyshouldbeinsertedhere' % (location)
-    full_url = url + location + key
+    url = 'http://api.openweathermap.org/data/2.5/forecast?q=%s&APPID=' % (location)
+    full_url = url + api_key
     return full_url
 
 
-def fetcher(full_url):
-    '''Fetch the weather forecast'''
+def main():
+    '''Main function'''
     try:
-        response = requests.get(url)
-        response.raise_for_status()
+        response = requests.get(url_builder(api_key))
+        response.raise_for_status
     except requests.exceptions.HTTPError:
         print('Access denied')
 
-
-# load json data into a variable
-def data_loader():
-    '''Load the json data to a variable and print it out'''
+    # load json data into a variable
     weatherData = json.loads(response.text)
     w = weatherData['list']
     print('Current weather in %s:' % (location))
@@ -56,16 +45,5 @@ def data_loader():
     print()
     print('Day after tomorrow:')
     print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])
-
-
-def get_weather_data(location):
-    '''Fetch weather data for a location'''
-    pass
-
-
-def main():
-    '''Main function'''
-    pass
-
 
 main()
